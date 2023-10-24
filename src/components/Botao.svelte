ce3fe695117ef34936ca947a7fe9d12d78225118
Pedro Henrique Ramos <pedrohscramos@gmail.com>
2023-10-24 13:53:41 -0300

    Commit Inicial

---
+++ src/components/Botao.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  export let texto = "";
+  export let triplo: boolean = false;
+  export let duplo: boolean = false;
+  export let operacao: boolean = false;
+  export let destaque: boolean = false;
+  export let onClick: (valor: string) => void = () => {};
+</script>
+
+<button
+  class="botao"
+  class:triplo
+  class:duplo
+  class:operacao
+  class:destaque
+  on:click={() => onClick(texto)}
+>
+  {texto}
+</button>
+
+<style>
+  .botao {
+    margin: 0;
+    border: 1px solid var(--cor-fundo);
+    flex-basis: 25%;
+  }
+
+  .duplo {
+    flex-basis: 50%;
+  }
+
+  .triplo {
+    flex-basis: 75%;
+  }
+
+  .operacao {
+    background-color: orange;
+    color: white;
+  }
+
+  .destaque {
+    background-color: red;
+    color: white;
+  }
+</style>
